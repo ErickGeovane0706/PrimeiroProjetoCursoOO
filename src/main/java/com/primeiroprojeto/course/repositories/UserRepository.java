b9966474575439fb609a47bd599b5c8ceeae29a4
Erick <erickgeovane2002@gmail.com>
2025-09-15 19:14:21 -0300

    Service layer, component registration

--- src/main/java/com/primeiroprojeto/course/repositories/UserRepository.java
+++ src/main/java/com/primeiroprojeto/course/repositories/UserRepository.java
@@ -3,5 +3,7 @@ package com.primeiroprojeto.course.repositories;
 import com.primeiroprojeto.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
---
+++ src/main/java/com/primeiroprojeto/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.primeiroprojeto.course.services;
+
+import com.primeiroprojeto.course.entities.User;
+import com.primeiroprojeto.course.repositories.UserRepository;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> finAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+}
