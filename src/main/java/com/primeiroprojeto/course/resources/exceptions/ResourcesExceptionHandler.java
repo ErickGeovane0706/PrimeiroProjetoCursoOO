bd15811b36d7573e0cb01ac5dcf669b07fd83546
Erick <erickgeovane2002@gmail.com>
2025-09-28 14:32:49 -0300

    Tramamento da Exceção Delete, erro 204, para erro Data Base Error e msg do DataBaseException obs:achei um pouco complicado

--- src/main/java/com/primeiroprojeto/course/resources/exceptions/ResourcesExceptionHandler.java
+++ src/main/java/com/primeiroprojeto/course/resources/exceptions/ResourcesExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.primeiroprojeto.course.resources.exceptions;
 
+import com.primeiroprojeto.course.services.exceptions.DataBaseException;
 import com.primeiroprojeto.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,13 @@ public class ResourcesExceptionHandler{
     public ResponseEntity<StandardError>resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         String erro="Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err= new StandardError(Instant.now(),status.value(),erro,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError>dataBase(DataBaseException e, HttpServletRequest request){
+        String erro="Data Base Error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err= new StandardError(Instant.now(),status.value(),erro,e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
